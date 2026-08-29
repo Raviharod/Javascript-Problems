16193bc1fd2ce35ec0c7076a1e2480676375f426
Ravi Harod <raviharod7828@gmail.com>
2026-08-29 12:27:18 +0530

    bubble sort implementation

--- functions.js
+++ functions.js
@@ -396,3 +396,29 @@ function firstNonRepChar(str){
 }
 str = "azbacddbecg";
 // console.log(firstNonRepChar(str));
+
+/*24. Bubble Sort Algorithm
+Write a JavaScript function to apply the Bubble Sort algorithm.
+Note : According to wikipedia "Bubble sort, sometimes referred to as sinking sort, is a simple sorting algorithm that works by repeatedly stepping through the list to be sorted, comparing each pair of adjacent items and swapping them if they are in the wrong order".
+
+Sample array : [12, 345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 213]
+Expected output : [3223, 546, 455, 345, 234, 213, 122, 98, 84, 64, 23, 12, 9, 4, 1] */
+function bubbleSort(arr) {
+  let n = arr.length;
+  let swapped;
+  for (let i = 0; i < n - 1; i++) {
+      swapped = false;
+      for (let j = 0; j < n - i - 1; j++) {
+          if (arr[j] > arr[j + 1]) {
+              [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+              swapped = true;
+          }
+      }
+      
+      if (!swapped) break;
+  }
+  return arr;
+}
+
+const unsortedArray = [12, 345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 213];
+// console.log("Sorted array:", bubbleSort(unsortedArray)); 
